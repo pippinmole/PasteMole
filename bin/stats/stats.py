05efa6b0180c21090a8163ce2ca710c002d64da7
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2019-12-23 14:14:04 +0000

    Update stats.py

--- bin/stats/stats.py
+++ bin/stats/stats.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 def ExecuteSQL(sql):
-    db = sqlite3.connect("../codeBlocks.db")
+    db = sqlite3.connect("../bin/codeBlocks.db")
     cursor = db.cursor()
 
     data = cursor.execute(sql).fetchall()
