2172be77359894c6dc18bf88116d5a62d130ecf6
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2019-12-23 12:55:48 +0000

    Update databaseFunctions.js

--- bin/server/databaseFunctions.js
+++ bin/server/databaseFunctions.js
@@ -2,18 +2,18 @@ const sqlite = require("sqlite3").verbose();
 
 const DATABASE_PATH = "codeBlocks.db";
 
-function OpenDatabase() {
+function OpenDatabase(callback) {
   this.db = new sqlite.Database(DATABASE_PATH, (err) => {
 
     if(err) {
       console.log("Could not connect to database: " + err);
       return null;
     } else {
-      return this.db;
+      callback(this.db);
     }
   });
 
-  return this.db;
+  this.db.close();
 }
 
 module.exports = {
@@ -34,18 +34,15 @@ module.exports = {
 
     let sql_entry = "INSERT INTO codeBlocks (url, name, description, code, codeType) VALUES ($url, $name, $description, $code, $codeType)";
 
-    let db = OpenDatabase();
-
-    db.run(sql_entry, {
-      $url: data.url,
-      $name: data.pasteName,
-      $description: data.pasteDescription,
-      $code: data.code,
-      $codeType: data.codeType
+    OpenDatabase((db) => {
+      db.run(sql_entry, {
+        $url: data.url,
+        $name: data.pasteName,
+        $description: data.pasteDescription,
+        $code: data.code,
+        $codeType: data.codeType
+      });
     });
-    db.run('COMMIT TRANSACTION;');
-
-    // TODO: Close
   },
   // Creates the database
   CreateDatabase: function() {
@@ -53,42 +50,37 @@ module.exports = {
 
     sql_entry = "CREATE TABLE IF NOT EXISTS codeBlocks (url TEXT PRIMARY KEY, name TEXT, description TEXT, code TEXT, codeType TEXT)";
 
-    let db = OpenDatabase();
-    db.run(sql_entry);
-    db.run('COMMIT TRANSACTION;');
-  },
-  Internal_OpenDatabase: function(sql) {
-    return OpenDatabase(sql);
+    OpenDatabase((db) => {
+      db.run(sql_entry);
+    });
   },
   getPaste: function(url, callback) {
     var data = [];
 
-    let db = new sqlite.Database(DATABASE_PATH);
+    OpenDatabase((db) => {
+      db.serialize(function() {
+        let sql = "SELECT * FROM codeBlocks WHERE url=$url";
 
-    db.serialize(function() {
-      let sql = "SELECT * FROM codeBlocks WHERE url=$url";
-
-      db.each(sql, [url], function(err, row) {
-        data.push(row);
-      }, function() {
-        db.close();
-        callback(data);
+        db.each(sql, [url], function(err, row) {
+          data.push(row);
+        }, () => {
+          callback(data);
+        });
       });
     });
   },
   getTableCount: function(callback) {
     var count = 0;
 
-    let db = new sqlite.Database("codeBlocks.db");
+    OpenDatabase((db) => {
+      db.serialize(function() {
+        let sql = "SELECT * FROM codeBlocks";
 
-    db.serialize(function() {
-      let sql = "SELECT * FROM codeBlocks";
-
-      db.each(sql, function(err, row) {
-        count += 1;
-      }, function() {
-        db.close();
-        callback(count);
+        db.each(sql, function(err, row) {
+          count += 1;
+        }, function() {
+          callback(count);
+        });
       });
     });
   }
