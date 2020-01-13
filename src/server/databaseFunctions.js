600bb0975c604a5a13782b5d4ccdb5e1364c2d7c
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2020-01-13 20:42:34 +0000

    Update databaseFunctions.js

--- src/server/databaseFunctions.js
+++ src/server/databaseFunctions.js
@@ -1,9 +1,10 @@
 const sqlite = require("sqlite3").verbose();
+const path = require('path')
 
-const DATABASE_PATH = "./databases/codeBlocks.db";
+const DATABASE_PATH = "/databases/codeBlocks.db";
 
 function OpenDatabase(callback) {
-  const db = new sqlite.Database(DATABASE_PATH, sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE, (err) => {
+  const db = new sqlite.Database(path.resolve(__dirname, DATABASE_PATH), sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE, (err) => {
     if(!err){
      console.log("Successfully created DB file: " + DATABASE_PATH);
     } else {
