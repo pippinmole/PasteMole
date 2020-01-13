b1ce1f642995c88078185bec41d32494500192f8
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2020-01-13 21:20:27 +0000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -70,6 +70,9 @@ app.get("/p/:id", (request, response) => {
     } else {
       const paste = rows[0];
 
+      console.log(paste);
+      console.log(JSON.stringify(paste));
+
       // Send to code template (with paste object)
       const _render = {
         pasteName: paste.name,
