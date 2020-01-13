0cd3ba886cafc6861f79a4541c1bb48a540c3c4a
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2020-01-13 17:35:32 +0000

    Update databaseFunctions.js

--- src/server/databaseFunctions.js
+++ src/server/databaseFunctions.js
@@ -44,11 +44,14 @@ module.exports = {
   },
   // Creates the database
   CreateDatabase: function() {
-    console.log("Creating the database schema: " + __dirname + "/"+ DATABASE_PATH);
+    console.log("Creating the database schema: " + __dirname + "/" + DATABASE_PATH);
 
     sql_entry = "CREATE TABLE IF NOT EXISTS codeBlocks (url TEXT PRIMARY KEY, name TEXT, description TEXT, code TEXT, codeType TEXT, passworded BOOLEAN)";
 
     OpenDatabase((db) => {
+
+      console.log(db);
+      
       db.run("BEGIN;")
 
       db.run(sql_entry);
